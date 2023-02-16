b1ec95a6a9414a04a5f917a79280e684b0edf0df
iksanh <iksanhariji@gmail.com>
2023-02-16 11:39:14 +0800

    add template about.html home.html  end confi in url and views

--- job_control/views.py
+++ job_control/views.py
@@ -1,8 +1,33 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from django.shortcuts import render
 
 # Create your views here.
 
+#data
+posts = [
+    {
+        'title': 'Beautiful is better than ugly',
+        'author': 'John Doe',
+        'content': 'Beautiful is better than ugly',
+        'published_at': 'October 1, 2022'
+    },
+    {
+        'title': 'Explicit is better than implicit',
+        'author': 'Jane Doe',
+        'content': 'Explicit is better than implicit',
+        'published_at': 'October 1, 2022'
+    }
+]
 
 def home(request):
-    return HttpResponse('<h1>Hello</h1>')
+    # return HttpResponse('<h1>Hello</h1>')
+    context = {
+        'posts' : posts,
+        'title' : 'List Job'
+    }
+    return render(request, "jobs/home.html", context)
+
+
+def about(request):
+    return render(request, "jobs/about.html")
