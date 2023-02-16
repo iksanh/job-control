83c07d909fabef2345bfa997797c2597936e58e8
iksanh <iksanhariji@gmail.com>
2023-02-16 13:47:01 +0800

    fix timezone now

--- job_control/models.py
+++ job_control/models.py
@@ -1,5 +1,6 @@
 from django.db import models
-from django.utils import  timezone
+from django.utils import timezone
+from django.contrib.auth.models import User
 
 # Create your models here.
 
@@ -7,7 +8,13 @@ class Job(models.Model):
     job_id = models.BigAutoField(primary_key=True)
     job_title = models.CharField(max_length=120)
     job_description = models.TextField()
-    job_start = models.DateTimeField(default=timezone.now())
+    job_publish = models.DateTimeField(default=timezone.now)
+    job_author = models.ForeignKey(User, on_delete=models.CASCADE)
 
     class Meta:
         db_table = 'm_job'  #specify table name in db
+        ordering = ['-job_publish']  # sort  job
+
+    def __str__(self):
+        return self.job_title
+
