93c78c31d5bc9bf998118ca32e66f75cd4a0697c
iksanh <iksanhariji@gmail.com>
2023-02-16 13:31:09 +0800

    add model job

--- job_control/models.py
+++ job_control/models.py
@@ -1,3 +1,13 @@
 from django.db import models
+from django.utils import  timezone
 
 # Create your models here.
+
+class Job(models.Model):
+    job_id = models.BigAutoField(primary_key=True)
+    job_title = models.CharField(max_length=120)
+    job_description = models.TextField()
+    job_start = models.DateTimeField(default=timezone.now())
+
+    class Meta:
+        db_table = 'm_job'  #specify table name in db
