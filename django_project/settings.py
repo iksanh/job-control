d8af8123b4ec8bdf42f6805c3fe7911c320da987
iksanh <iksanhariji@gmail.com>
2023-02-17 09:58:34 +0800

    add module users in settings

--- django_project/settings.py
+++ django_project/settings.py
@@ -37,7 +37,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'job_control' #add project apps
+    'job_control', #add module job
+    'users' #add module users
 ]
 
 MIDDLEWARE = [
