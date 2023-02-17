2b461453504f199d1eb0aabb846157af531c2bf6
iksanh <iksanhariji@gmail.com>
2023-02-18 05:38:40 +0800

    add class RegisterForm

--- users/views.py
+++ users/views.py
@@ -1,5 +1,5 @@
 from django.shortcuts import render, redirect
-from .forms import LoginForm
+from .forms import LoginForm, RegisterForm
 from django.contrib import messages
 from django.contrib.auth import login, authenticate, logout
 
@@ -38,5 +38,26 @@ def sign_out(request):
     logout(request)
     messages.success(request, 'You have been logout')
     return redirect('login')
+
+
+def sign_up(request):
+    if request.method == 'GET':
+        form = RegisterForm()
+        return  render(request, 'users/registration.html', {'form' : form})
+
+    if request.method == 'POST':
+
+        form = RegisterForm(request.POST)
+
+        if form.is_valid():
+            user = form.save(commit=False)
+            user.username = user.username.lower()
+            user.save()
+            messages.success(request, 'You have singeng up successfully')
+            login(request, user)
+            return redirect('home')
+
+        else:
+            return render(request, 'users/registration.html', {'form' :  form})
 
 
