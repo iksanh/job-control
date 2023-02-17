e3e903e1200accbc5b14d1f74c7018722b9b2aaa
iksanh <iksanhariji@gmail.com>
2023-02-17 09:48:39 +0800

    add route for edit and delete job

--- job_control/urls.py
+++ job_control/urls.py
@@ -3,5 +3,8 @@ from . import views
 
 urlpatterns = [
     path('', views.home , name='home'),
-    path('about/', views.about , name='about'),
+    path('create/', views.create_job , name='job-create'),
+    path('edit/<int:id>/', views.edit_job , name='job-edit'),
+    path('delete/<int:id>/', views.delete_job , name='job-delete'),
+    path('about/', views.about , name='about')
 ]
--- job_control/views.py
+++ job_control/views.py
@@ -1,6 +1,9 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect, get_object_or_404
 from django.http import HttpResponse
+from django.contrib import messages
 from django.shortcuts import render
+from .models import Job
+from .form import JobForm
 
 # Create your views here.
 
@@ -20,12 +23,58 @@ posts = [
     }
 ]
 
+def create_job(request):
+    if request.method == 'GET':
+        context = {'form' : JobForm()}
+        return render(request, 'jobs/job_form.html', context)
+    elif request.method == 'POST':
+        form = JobForm(request.POST)
+        if form.is_valid():
+            form.save()
+            messages.success(request, 'New job has add successfully')
+            return redirect('home')
+        else:
+            messages.error(request, 'Failed to add job')
+            return render(request, 'jobs/job_form.html', {'form': form})
+
+def edit_job(request, id):
+    post = get_object_or_404(Job, job_id=id)
+    if request.method =='GET':
+        context = {'form' : JobForm(instance=post), 'id' : id}
+        return render(request, 'jobs/job_form.html', context)
+
+    if request.method == 'POST':
+        form = JobForm(request.POST, instance=post)
+        if form.is_valid():
+            form.save()
+            messages.success(request, 'Edit data succesfully  ')
+            return redirect('home')
+        else:
+            messages.error(request,'Please correct error input')
+            return render(request, 'jobs/job_form.html', {'form' : form})
+
+def delete_job(request, id):
+    job  = get_object_or_404(Job, job_id = id)
+    context = {'job' : job}
+
+    if request.method == 'GET':
+        return render(request, 'jobs/job_confirm_delete.html', context)
+    elif request.method == 'POST':
+        job.delete()
+        messages.success(request, 'The Job has been delete successfully')
+        return redirect('home')
+
+
 def home(request):
+
+    jobs  = Job.objects.all()
     # return HttpResponse('<h1>Hello</h1>')
     context = {
-        'posts' : posts,
+        'jobs' : jobs,
         'title' : 'List Job'
     }
+
+    
     return render(request, "jobs/home.html", context)
 
 
