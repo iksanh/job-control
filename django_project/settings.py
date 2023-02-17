dfbdaf0a924916a77ca1e6dd30a0fe79ec37204a
iksanh <iksanhariji@gmail.com>
2023-02-18 04:34:17 +0800

    add LOGIN_URL in settings

--- django_project/settings.py
+++ django_project/settings.py
@@ -127,3 +127,5 @@ STATICFILES_DIRS = [BASE_DIR / 'static']
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+LOGIN_URL = 'login'
